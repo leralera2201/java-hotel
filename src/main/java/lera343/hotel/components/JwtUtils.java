0664874793188856d592635e23b7e18762222439
Lera <leravodanchuk@gmail.com>
2021-04-25 18:22:42 +0300

    token issues

--- src/main/java/lera343/hotel/components/JwtUtils.java
+++ src/main/java/lera343/hotel/components/JwtUtils.java
@@ -3,7 +3,6 @@ package lera343.hotel.components;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import java.security.Key;
 import java.util.Date;
 
 import lera343.hotel.entity.security.UserDetailsImpl;
@@ -11,33 +10,37 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+
 @Component
 @RequiredArgsConstructor
 public class JwtUtils {
+    public static final SecretKey KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
     public String generateJwtToken(Authentication authentication) {
         var principal = (UserDetailsImpl) authentication.getPrincipal();
 
         return Jwts.builder()
                 .setSubject(principal.getUsername())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime()+1000000000))
-                .signWith(Keys.secretKeyFor(SignatureAlgorithm.HS512))
+                .setExpiration(new Date(new Date().getTime()+10000000))
+                .signWith(KEY)
                 .compact();
     }
 
     public String getUsernameFromJwtToken(String token) {
         return Jwts.parser()
-                .setSigningKey(Keys.secretKeyFor(SignatureAlgorithm.HS512))
-                .parseClaimsJwt(token)
+                .setSigningKey(KEY)
+                .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
     }
 
     public boolean validate(String token){
         try{
+            token = token.replace("\"","");
             Jwts.parser()
-                    .setSigningKey(Keys.secretKeyFor(SignatureAlgorithm.HS512))
-                    .parseClaimsJwt(token);
+				.setSigningKey(KEY)
+                .parseClaimsJws(token);
             return true;
         }catch (Exception e){
 
